cf20945eaa9a8def97c3c6d3afbf2141cbb51378
salindupawan <salindupa@gmail.com>
2026-03-07 10:55:41 +0530

    initial commit

---
+++ src/main/java/com/salindupawan/skill_mentor_backend/SkillMentorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.salindupawan.skill_mentor_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillMentorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillMentorBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salindupawan/skill_mentor_backend/SkillMentorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salindupawan.skill_mentor_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillMentorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
