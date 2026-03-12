5b94244e914321ca55a309d3d534c1fe1f992c70
salindupawan <salindupa@gmail.com>
2026-03-12 13:22:30 +0530

    fix: update allowed origins

--- src/main/java/com/salindupawan/skill_mentor_backend/config/CorsConfig.java
+++ src/main/java/com/salindupawan/skill_mentor_backend/config/CorsConfig.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Configuration
 public class CorsConfig {
 
-    @Value("${cors.allowed-origins:http://localhost:3000,http://localhost:5173,http://localhost:8080}")
+    @Value("${cors.allowed-origins:http://localhost:3000,http://localhost:5173,http://localhost:8080,https://skilledmentor.vercel.app/}")
     private String allowedOrigins;
 
     @Bean
