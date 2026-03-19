75e76f822f1b4bca901ed8a92af99504e909234d
salindupawan <salindupa@gmail.com>
2026-03-20 00:25:51 +0530

    chore: update patch session request

--- src/main/java/com/salindupawan/skill_mentor_backend/dto/request/PatchSessionRequest.java
+++ src/main/java/com/salindupawan/skill_mentor_backend/dto/request/PatchSessionRequest.java
@@ -2,11 +2,15 @@ package com.salindupawan.skill_mentor_backend.dto.request;
 
 import com.salindupawan.skill_mentor_backend.entity.PaymentStatus;
 import com.salindupawan.skill_mentor_backend.entity.SessionStatus;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PatchSessionRequest {
     private String meetingLink;
     private SessionStatus sessionStatus;
